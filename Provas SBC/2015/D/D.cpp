b7023637633fb944cbd14b537a72c9b0b94f3ae2
Gabriel Sousa <gabrielssc.ti@gmail.com>
2020-03-09 11:54:30 -0300

    Finished D

--- Provas SBC/2015/D/D.cpp
+++ Provas SBC/2015/D/D.cpp
@@ -2,32 +2,70 @@
 
 using namespace std;
 
-typedef struct{
-    string var;
-    int val;
-} Variavel;
+string var[120][120];
+int lineSum[120], colSum[120];
+int l, c;
 
-string data[110][110];
-int resLinha[110];
-int resCol[110];
-vector<Variavel> variaveis;
-
-bool isVariavelEqual(Variavel v){
-    for(Variavel i : variaveis){
-        if(!i.var.compare(v.var))
-            return true;
+void update(string &found, int val){
+    for(int i=0; i<l; i++){
+        for(int j=0; j<c; j++){
+            if(var[i][j]==found){
+                lineSum[i] -= val;
+                colSum[j] -= val;
+            }
+        }
     }
 }
-
-bool isVariavelSet(Variavel v){
-    return (v.val==INT32_MAX);
-}
-
-int l, c;
 
 int main(){
     cin >> l >> c;
+    set<string> unknown;
     for(int i=0; i<l; i++){
-        
+        for(int j=0; j<c; j++){
+            cin >> var[i][j];
+            unknown.insert(var[i][j]);
+        }
+        cin >> lineSum[i];
     }
+    for(int j=0; j<c; j++)
+        cin >> colSum[j];
+
+    map<string, int> known;
+    while(known.size()!=unknown.size()){
+        for(int i=0; i<l; i++){
+            set<string> unk;
+            int times = 0;
+            for(int j=0; j<c; j++){
+                if(!known.count(var[i][j])){
+                    times++;
+                    unk.insert(var[i][j]);
+                }
+            }
+            if(unk.size()==1){
+                string found = *unk.begin();
+                known[found] = lineSum[i]/times;
+                update(found, known[found]);
+            }
+        }
+        for(int j=0; j<c; j++){
+            set<string> unk;
+            int times = 0;
+            for(int i=0; i<l; i++){
+                if(!known.count(var[i][j])){
+                    times++;
+                    unk.insert(var[i][j]);
+                }
+            }
+            if(unk.size()==1){
+                string found = *unk.begin();
+                known[found] = colSum[j] / times;
+                update(found, known[found]);
+                break;
+            }
+        }
+    }
+    for(auto &res : known)
+        cout << res.first << " " << res.second << endl;
+    
+    return 0;
 }
